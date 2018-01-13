38624afb8064f510b74e606a5fc0f61b0991b0b0
eches <Elaine.Chesoni83@gmail.com>
2018-01-13 13:41:31 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+//hit cmd+opt+j to see console log on mac
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var checkForMatch = function(){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again");
+  }
+};
+
+var flipCard = function(cardId){
+
+  console.log("User flipped " + cards[cardId]);
+
+  cardsInPlay.push(cards[cardId]);
+
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+};
+
+flipCard(0);
+flipCard(2);
