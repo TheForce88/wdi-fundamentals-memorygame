bcf59572371e954674cf512b10d043ddd38d3dd6
eches <Elaine.Chesoni83@gmail.com>
2018-01-15 00:37:54 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,22 +23,42 @@ var cards = [
   }
 ];
 var cardsInPlay = [];
+var score = 0;
+var result = document.getElementById("match-or-not"); //changes message after match or not
+var clickedCards = document.getElementsByClassName("clicked"); // keeps track of which cards have been selected
 
-var checkForMatch = function(){
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  }
-  else {
-    alert("Sorry, try again");
+//write function to append card to clickedCards every time card is clicked
+
+//flips back cards after they have been flipped
+var flipBack = function (){
+  for (var i=0; i < clickedCards.length; i+=1){
+    clickedCards[i].setAttribute("src","images/back.png");
   }
 };
 
-var flipCard = function(cardId){
+//checks if cards are a match, if not resets cardsInPlay
+var checkForMatch = function(){
+  if (cardsInPlay.length == 2){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      result.textContent = "You found a match!";
+      score += 1;
+      document.getElementById("score").textContent = score;
+    }
+    else {
+      result.textContent = "Sorry, try again.";
+    }
+  }
+  cardsInPlay = [];
+};
 
-  console.log("User flipped " + cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
+//whenever we click a card, card is flipped over -- displays cardImage
+var flipCard = function() {
 
+  var cardId = this.getAttribute("data-id");
+  var card = cards[cardId];
+  this.setAttribute("src",card.cardImage);
+  this.setAttribute("class", "clicked");
+  console.log(this);
   cardsInPlay.push(cards[cardId].rank);
 
   if (cardsInPlay.length === 2) {
@@ -46,5 +66,24 @@ var flipCard = function(cardId){
   }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+};
+
+//starts game over
+var resetGame = function () {
+  score = 0;
+  document.getElementById("score").textContent = score;
+  result.textContent = "Start Over?";
+  flipback();
+};
+
+//begins program by creating board and waiting for reset
+createBoard();
+document.querySelector("button").addEventListener("click", resetGame);
